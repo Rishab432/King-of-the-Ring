9ab1af357a792a865bd0c40e06ec2138dd2512de
Rishab Areekkal <rishab.areekkal24@ycdsbk12.ca>
2023-09-26 09:27:24 -0400

    random changes

--- Assets/Script/Countdown.cs
+++ Assets/Script/Countdown.cs
@@ -1,20 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class Countdown : MonoBehaviour
 {
+    public static Countdown Instance;
+    public GameObject _text_holder;
     [SerializeField] private TMP_Text _countdown_title;
     public static bool gameStart;
-    
-    void Start()
+
+    void Awake()
     {
-        StartCoroutine(readySetGoTimer(1));
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    void Update()
+    {
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+
+        if (sceneName == "FightScene")
+        {
+            StartCoroutine(readySetGoTimer(1));
+        }
+    }
+
+    //void OnEnable()
+    //{
+    //    SceneManager.sceneLoaded += OnSceneLoaded;
+    //}
+
+    //void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //{
+    //    if (scene.name == "FightScene")
+    //    {
+    //        StartCoroutine(readySetGoTimer(1));
+    //    }
+    //}
+
+    //void OnDisable()
+    //{
+    //    SceneManager.sceneLoaded -= OnSceneLoaded;
+    //}
 
     private IEnumerator readySetGoTimer(float wait)
     {
+        _text_holder.SetActive(true);
         PauseMenu.Instance.pauseButton.SetActive(false);
         gameStart = false;
         _countdown_title.text = "Ready";
@@ -26,6 +70,6 @@ public class Countdown : MonoBehaviour
         _countdown_title.text = "";
         PauseMenu.Instance.pauseButton.SetActive(true);
         gameStart = true;
-        Destroy(_countdown_title);
+        _text_holder.SetActive(false);
     }
 }
